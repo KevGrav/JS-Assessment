6139ddbabda8459931a144d87b18cce6781757f0
Kev Grav <kgravell.prsvr@gmail.com>
2024-01-10 16:48:18 -0500

    updated readme, worked on js

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 This project is set out to make a vending machine using HTML, CSS and JavaScript.
 The vending machine will have 12 different snack options.
-First we will need to load our 12 snacks using the vending options. Each should have a corresponding image that will display in one of the vending boxes.
+Images of 12 different snacks as vending options. Each should have a corresponding identity/item code.
 The screen will display "Make a selection".
 Use the keyboard to select the desired snack reference code. The buttons are interactive, as they are pressed their values are displayed on the screen.
-The snack will be dispensed into the drawer.
+The snack will be dispensed into the drawer/drop box below.
 
+Advanced - if time
+give option to load different snacks from [inventory]
+Add cash loading , prices to each of snacks, verify if sufficient funds, return change
--- main.js
+++ main.js
@@ -8,7 +8,106 @@ const THREE = '3';
 const FOUR = '4';
 
 //Variables
-let itemCode = "";
+let itemCode = '';
 
 
-//
+//Display screen/window
+const codeWindow_div = document.getElementById('code-window');
+
+//Assign code and control buttons
+const a_button = document.getElementById('letter-a');
+const b_button = document.getElementById('letter-b');
+const c_button = document.getElementById('letter-c');
+const one_button = document.getElementById('number-1');
+const two_button = document.getElementById('number-2');
+const three_button = document.getElementById('number-3');
+const four_button = document.getElementById('number-4');
+const select_button = document.getElementById('select');
+const clear_button = document.getElementById('clear');
+
+//Code and control button listners
+a_button.addEventListener('click', function () {
+    console.log(A + ' pressed');
+    addToItemCode(A);
+});
+  
+b_button.addEventListener('click', function () {
+    console.log(B + ' pressed');
+    addToItemCode(B);
+});
+  
+c_button.addEventListener('click', function () {
+    console.log(C + ' pressed');
+    addToItemCode(C);
+});
+  
+one_button.addEventListener('click', function () {
+    console.log(ONE + ' pressed');
+    addToItemCode(ONE);
+});
+  
+two_button.addEventListener('click', function () {
+    console.log(TWO + ' pressed');
+    addToItemCode(TWO);
+});
+  
+three_button.addEventListener('click', function () {
+    console.log(THREE + ' pressed');
+    addToItemCode(THREE);
+});
+  
+four_button.addEventListener('click', function () {
+    console.log(FOUR + ' pressed');
+    addToItemCode(FOUR);
+});
+
+select_button.addEventListener('click', function () {
+    console.log('select pressed');
+    selectItem();
+});
+  
+clear_button.addEventListener('click', function () {
+    console.log('clear pressed');
+    itemCode = '';
+    printItemCode();
+}); 
+
+// function to check if character is valid. If yes add to itemCode
+function addToItemCode(char) {
+    if (itemCode.length < 2) {
+      if (itemCode.length < 1 && isLetter(char)) {
+        itemCode += char;
+      }
+      if (itemCode.length == 1 && !isLetter(char)) {
+        itemCode += char;
+      }
+    }
+    // console.log('item code ' + itemCode);
+    printItemCode();
+    if (itemCode.length == 2) {
+      printOutput(itemCode);
+    }
+}
+
+// function to check if character is a letter. true if yes
+function isLetter(char) {
+    return char.match(/[a-z]/i);
+}
+
+// function to display item code in item code screen/window
+function printItemCode() {
+    codeWindow_div.innerHTML = itemCode;
+}
+
+// function to place selected item/snack into drop box
+function selectItem(){
+    if (itemCode.length == 2) {
+        let snack = itemCode;
+        document.querySelectorAll("#snack");
+        }
+}
+
+
+
+
+
