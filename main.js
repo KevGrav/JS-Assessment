8e6e722c840d7a521d7cf0a830d2b578c1629f0f
Kev Grav <kgravell.prsvr@gmail.com>
2024-01-10 13:12:33 -0500

    first complete html and css, initial js

---
+++ main.js
@@ -0,0 +1,14 @@
+//Button constants
+const A = 'A';
+const B = 'B';
+const C = 'C';
+const ONE = '1';
+const TWO = '2';
+const THREE = '3';
+const FOUR = '4';
+
+//Variables
+let itemCode = "";
+
+
+//
