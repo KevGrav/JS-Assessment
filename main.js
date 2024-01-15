fdb1957e33ecae3ea4d77001a003de9407cf2c14
Kev Grav <kgravell.prsvr@gmail.com>
2024-01-15 18:53:28 -0500

    small style adjustments, added clear button

--- main.js
+++ main.js
@@ -1,85 +1,56 @@
-function A1(){
+// Function to play sound as image of selected snack is placed in the drop box 
+  function playAudioAndChangeImage(imageSource) {
     let audio = new Audio('../sound/vendMac.mp3');
     audio.play();
+
     setTimeout(() => {
-        document.getElementById('myImage').src = "images/A1.jpg"
-     },4600)
-  }
-    
-function A2(){
-    let audio = new Audio('../sound/vendMac.mp3');
-    audio.play();
-    setTimeout(() => {
-    document.getElementById('myImage').src = 'images/A2.jpg';
-        },4600)
-  }
-function A3(){
-    let audio = new Audio('../sound/vendMac.mp3');
-    audio.play();
-    setTimeout(() => {
-    document.getElementById('myImage').src = 'images/A3.jpg';
-        },4600)
-  }
-function A4(){
-    let audio = new Audio('../sound/vendMac.mp3');
-    audio.play();
-    setTimeout(() => {
-    document.getElementById('myImage').src = 'images/A4.jpg';
-        },4600)
-  }
-function B1(){
-    let audio = new Audio('../sound/vendMac.mp3');
-    audio.play();
-    setTimeout(() => {
-    document.getElementById('myImage').src = 'images/B1.jpg';
-        },4600)
+        document.getElementById('myImage').src = imageSource;
+    }, 4600);
 }
-function B2(){
-    let audio = new Audio('../sound/vendMac.mp3');
-    audio.play();
+
+function A1() {
+    playAudioAndChangeImage('images/A1.jpg');
+}
+
+function A2() {
+    playAudioAndChangeImage('images/A2.jpg');
+}
+
+function A3() {
+    playAudioAndChangeImage('images/A3.jpg');
+}
+
+function A4() {
+    playAudioAndChangeImage('images/A4.jpg');
+}
+function B1() {
+    playAudioAndChangeImage('images/B1.jpg');
+}
+function B2() {
+    playAudioAndChangeImage('images/B2.jpg');
+}
+function B3() {
+    playAudioAndChangeImage('images/B3.jpg');
+}
+function B4() {
+    playAudioAndChangeImage('images/B4.jpg');
+}
+function C1() {
+    playAudioAndChangeImage('images/C1.jpg');
+}
+function C2() {
+    playAudioAndChangeImage('images/C2.jpg');
+}
+function C3() {
+    playAudioAndChangeImage('images/C3.jpg');
+}
+function C4() {
+    playAudioAndChangeImage('images/C4.jpg');
+}
+
+// function to clear image from drop box
+function Clear(){
     setTimeout(() => {
-    document.getElementById('myImage').src = 'images/B2.jpg';
-        },4600)
-  }
-function B3(){
-    let audio = new Audio('../sound/vendMac.mp3');
-    audio.play();
-    setTimeout(() => {
-    document.getElementById('myImage').src = 'images/B3.jpg';
-        },4600)
-  }
-function B4(){
-    let audio = new Audio('../sound/vendMac.mp3');
-    audio.play();
-    setTimeout(() => {
-    document.getElementById('myImage').src = 'images/B4.jpg';
-        },4600)
-  }
-function C1(){
-    let audio = new Audio('../sound/vendMac.mp3');
-    audio.play();
-    setTimeout(() => {
-    document.getElementById('myImage').src = 'images/C1.jpg';
-        },4600)
-  }
-function C2(){
-    let audio = new Audio('../sound/vendMac.mp3');
-    audio.play();
-    setTimeout(() => {
-    document.getElementById('myImage').src = 'images/C2.jpg';
-        },4600)
-  }
-function C3(){
-    let audio = new Audio('../sound/vendMac.mp3');
-    audio.play();
-    setTimeout(() => {
-    document.getElementById('myImage').src = 'images/C3.jpg';
-        },4600)
-  }
-function C4(){
-    let audio = new Audio('../sound/vendMac.mp3');
-    audio.play();
-    setTimeout(() => {
-    document.getElementById('myImage').src = 'images/C4.jpg';
-        },4600)
+    document.getElementById('myImage').src = 'images/myImage.jpg';
+        },10)
   }
